474a537e20ebf93b24568bb50a34fbfaaccee490
Max Bryselbout <max.bryselbout@gmail.com>
2016-04-27 22:52:12 +0200

    migrate to angular 1.5

--- src/app/common/directives/forms-directives/form-components/multiple-choice/form.multiplechoice.directive.js
+++ src/app/common/directives/forms-directives/form-components/multiple-choice/form.multiplechoice.directive.js
@@ -1,10 +1,21 @@
 (function(angular) {
+  function formMultipleChoiceCtrl() {
+    var ctrl = this;
+    this.selectChoice = function(choice) {
+      choice.selected = choice.selected ? false : true;
+    }
+    this.submitChoice = function() {
+      var choicesSelected = this.item.choices.filter(function(item) {
+        return item.selected;
+      });
+      console.log(choicesSelected);
+    }
+  }
   angular
     .module('app')
-    .directive("formMultipleChoice", function() {
-      return {
-        restrict: 'EA',
-        templateUrl: 'app/common/directives/forms-directives/form-components/multiple-choice/form.multiplechoice.tpl.html'
-      };
+    .component("formMultipleChoice", {
+        templateUrl: 'app/common/directives/forms-directives/form-components/multiple-choice/form.multiplechoice.tpl.html',
+        controller : formMultipleChoiceCtrl,
+        bindings: {item: '='}
     });
 })(angular);
