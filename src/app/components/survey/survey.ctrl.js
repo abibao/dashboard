8d694a67c20464fc3206ea24431cbcf1691b54b6
Gilles Perreymond <gperreymond@viadeoteam.com>
2016-09-29 10:49:39 +0200

    [1.2.7] add html to all descriptions' item. update CGU question.

--- src/app/components/survey/survey.ctrl.js
+++ src/app/components/survey/survey.ctrl.js
@@ -1,80 +1,74 @@
 (function(angular) {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('app')
-        .controller('surveyCtrl', surveyCtrl);
+  angular
+      .module('app')
+      .controller('surveyCtrl', surveyCtrl);
 
-    function surveyCtrl(Analytics, $rootScope, $scope, survey, abibaoApiSvc, $stateParams, $state, getNextState) {
-        Analytics.pageView();
-        survey.items.sort(function(a, b) {
-          return a.position - b.position;
-        });
-        $scope.progress = false;
-        var answsers = [];
-        $scope.screenWelcomeContent = survey.screenWelcomeContent;
-        $scope.hideScreenWelcomeContent = function() {
-          $scope.screenWelcomeContent = '';
-        }
-        $scope.progress = {
-            max: survey.items.length,
-            current: 1,
-            maxIndex: answsers.length + 1
-        };
-        $scope.$on("$stateChangeSuccess", function(event, toState, toParams) {
-            var index = parseInt(toParams.index || 1) - 1;
-            if (index < 0) {
-                $state.go('survey.step', {
-                    index: 1
-                });
-            }
-            $scope.item = survey.items[index];
-            $scope.progress.current = index + 1;
-            $scope.next = function() {
-                $state.go('survey.step', {
-                    index: ++index + 1
-                });
-            }
-            $scope.previous = function() {
-                $state.go('survey.step', {
-                    index: --index + 1
-                });
-            }
-        });
-
-        $scope.submitAnswer = function(response) {
-
-          if (response.label === 'ABIBAO_ANSWER_ASKING_CGU' && response.answer === 'no') {
-            // ici je ne fais rien
-            // mais on peut faire ce que l'on veut du coup !
-            return null;
+  function surveyCtrl(Analytics, $rootScope, $scope, survey, abibaoApiSvc, $stateParams, $state, getNextState) {
+      Analytics.pageView();
+      survey.items.sort(function(a, b) {
+        return a.position - b.position;
+      });
+      $scope.progress = false;
+      var answsers = [];
+      $scope.screenWelcomeContent = survey.screenWelcomeContent;
+      $scope.hideScreenWelcomeContent = function() {
+        $scope.screenWelcomeContent = '';
+      }
+      $scope.progress = {
+          max: survey.items.length,
+          current: 1,
+          maxIndex: answsers.length + 1
+      };
+      $scope.$on("$stateChangeSuccess", function(event, toState, toParams) {
+          var index = parseInt(toParams.index || 1) - 1;
+          if (index < 0) {
+              $state.go('survey.step', {
+                  index: 1
+              });
           }
-                abibaoApiSvc.survey.answers({
-                    urn: $stateParams.urn
-                }, {
-                    label: response.label,
-                    answer: response.answer
-                }, function() {
+          $scope.item = survey.items[index];
+          $scope.progress.current = index + 1;
+          $scope.next = function() {
+              $state.go('survey.step', {
+                  index: ++index + 1
+              });
+          }
+          $scope.previous = function() {
+              $state.go('survey.step', {
+                  index: --index + 1
+              });
+          }
+      });
 
-                    answsers.push(response);
-                    // console.log(answsers);
-                    if ($scope.progress.current >= $scope.progress.max) {
-                      // if (survey.screenThankYouContent) {
-                      //     $state.go('survey.screencontent', {
-                      //         content: survey.screenThankYouContent,
-                      //         type: 'thankYou'
-                      //     });
-                      // }
-                      // else {
-                        getNextState().then(function(nextState) {
-                            $state.go(nextState.stateName, nextState.params);
-                        });
-                      // }
-                    } else {
-                        $scope.progress.maxIndex = answsers.length + 1;
-                        $scope.next();
-                    }
+    $scope.submitAnswer = function(response) {
+        abibaoApiSvc.survey.answers({
+            urn: $stateParams.urn
+        }, {
+            label: response.label,
+            answer: response.answer
+        }, function() {
+
+            answsers.push(response);
+            // console.log(answsers);
+            if ($scope.progress.current >= $scope.progress.max) {
+              // if (survey.screenThankYouContent) {
+              //     $state.go('survey.screencontent', {
+              //         content: survey.screenThankYouContent,
+              //         type: 'thankYou'
+              //     });
+              // }
+              // else {
+                getNextState().then(function(nextState) {
+                    $state.go(nextState.stateName, nextState.params);
                 });
+              // }
+            } else {
+                $scope.progress.maxIndex = answsers.length + 1;
+                $scope.next();
             }
-    }
+        });
+      }
+  }
 })(angular);
